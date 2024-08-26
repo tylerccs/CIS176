8d6ce8dfd6fbaa206e80938c2af5d74e08e88a08
tylerccs <147091276+tylerccs@users.noreply.github.com>
2024-08-25 17:07:03 -0700

    Add files via upload

---
+++ CIS176_Cooper/Cooper_PE1.py
@@ -0,0 +1,19 @@
+# This is a program created by Tyler Cooper for CIS176 PE1
+
+def pstatements():
+    
+    print("Hello, Mike!")
+    print("Hello", "Mike!")
+    print(5)
+    print(5.0)
+    print(2 + 5)
+    print(2.0 + 5.0)
+    print("2" + "5")
+    print("2 + 5 = ", 2 + 5)
+    print(2 * 5)
+    print(5 ** 3)
+    print(11 / 3)
+    print(11 // 3)
+    
+    
+pstatements()
